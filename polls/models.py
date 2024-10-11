e39c7841dfaea039bf3347e170e788786c8026c2
Jovanni Feliz <feliz.jovanni@gmail.com>
2024-10-10 21:19:25 -0500

    Added models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+  question_text: str = models.CharField(max_length=200)
+  PUBLICATION_DATE = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+  question: str = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text: str = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
