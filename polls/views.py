4b264e6181a145bbe073370d874020311d739c90
Jovanni Feliz <feliz.jovanni@gmail.com>
2024-10-10 20:32:53 -0500

    Edited a View request

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello, everyone. You\'re at the Polls index!")
+  return HttpResponse("Hello, Jovanni. You\'re at the Polls index! Hooray!")
